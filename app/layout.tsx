f7012e3c8d3de804e3d80291ef4d18fb4e2b6374
gustafer <gustavobreasy@gmail.com>
2024-12-27 22:22:36 -0300

    fix: styling and now static gif is on assets folder

--- app/layout.tsx
+++ app/layout.tsx
@@ -39,7 +39,7 @@ export default function RootLayout({
         <Providers themeProps={{ attribute: "class", defaultTheme: "dark" }}>
           <div className="relative flex flex-col min-h-screen">
             {/* Background */}
-            <div className="absolute inset-0 bg-[url('https://cliply.co/wp-content/uploads/2021/07/402107790_STATIC_NOISE_400.gif')] bg-cover bg-center opacity-5 pointer-events-none z-0" />
+            <div className="absolute inset-0 bg-[url('/assets/static.gif')] bg-repeat bg-[length:300px_300px] opacity-5 pointer-events-none z-0" />
 
             {/* Main Content */}
             <main
