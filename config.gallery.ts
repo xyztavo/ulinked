3b90bbee18f43966175ce2833572d0465349185a
xyztavo <gustavobreasy@gmail.com>
2025-01-30 18:54:38 -0300

    fix: update gallery images and dependencies

--- config.gallery.ts
+++ config.gallery.ts
@@ -6,20 +6,40 @@ const gallery: { albums: Album[] } = {
       title: "me",
       route: "me",
       coverImageSrc:
-        "https://lh3.googleusercontent.com/p2A31eJoSiiH8aF6E7GRU-2vUJTeqNBTnnuo0OmSu_lI831St_sMU_D4vNd6_vFJvIISkFiRYJgwnmTs=w544-h544-l90-rj",
+        "/assets/me/me-cover.png",
       posts: [
         {
-          title: "skate",
-          src: "/assets/me/skate-yellow-flowers.jpg",
+          title: "1",
+          src: "/assets/me/1.png",
         },
         {
-          title: "focus",
-          src: "/assets/me/back.jpg",
-          videoSrc: "", /* if you wish to use a video, pass it right here, this one can be omitted */
+          title: "2",
+          src: "/assets/me/2.png",
         },
         {
-          title: "graffiti",
-          src: "/assets/me/grafitti.jpg", /* videoSrc is optional, so it can be omitted */
+          title: "3",
+          src: "/assets/me/3.png",
+        },
+        {
+          title: "4",
+          src: "/assets/me/4.png",
+        },
+        {
+          title: "5",
+          src: "/assets/me/5.png",
+        },
+        {
+          title: "6",
+          src: "/assets/me/6.png",
+        },
+        {
+          title: "7",
+          src: "/assets/me/7.png",
+        },
+        {
+          title: "8",
+          src: "/assets/me/8.png",
+          videoSrc: "", /* if you wish to use a video, pass it right here, this one can be omitted if you dont wish to */
         },
       ],
     },
