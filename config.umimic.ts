433b49a0845db9842df7bfbaf865318444c6e289
xyztavo <gustavobreasy@gmail.com>
2025-11-04 15:43:24 -0300

    feat: add bot

--- config.umimic.ts
+++ config.umimic.ts
@@ -2,6 +2,6 @@ import config from "./config";
 
 export const UmimicConfig = {
   // apiBaseUrl: "http://localhost:2000",
-  apiBaseUrl: "https://umimic-production.up.railway.app",
+  apiBaseUrl: "https://dip-production-6c90.up.railway.app",
   greeting: `Olá! Nós somos a ${config.nickname}, como posse ajudar você hoje?`
 };
