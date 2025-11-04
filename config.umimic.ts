ca5e0864ea60a151276261fd83d61e330a03dc63
xyztavo <gustavobreasy@gmail.com>
2025-11-04 16:10:14 -0300

    fix: typo

--- config.umimic.ts
+++ config.umimic.ts
@@ -3,5 +3,5 @@ import config from "./config";
 export const UmimicConfig = {
   // apiBaseUrl: "http://localhost:2000",
   apiBaseUrl: "https://dip-production-6c90.up.railway.app",
-  greeting: `Olá! Nós somos a ${config.nickname}, como posse ajudar você hoje?`
+  greeting: `Olá! Nós somos a ${config.nickname}, como posso ajudar você hoje?`
 };
